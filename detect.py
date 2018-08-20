c458335badc225091380230c5a1fd69dff78fac2
Charlie Lam <lin.xiaohan@mail.bnu.edu.cn>
2018-08-20 17:51:30 +0800

    first commit

--- detect.py
+++ detect.py
@@ -80,26 +80,6 @@ if __name__ ==  '__main__':
     args = arg_parse()
     
     scales = args.scales
-    
-    
-#        scales = [int(x) for x in scales.split(',')]
-#        
-#        
-#        
-#        args.reso = int(args.reso)
-#        
-#        num_boxes = [args.reso//32, args.reso//16, args.reso//8]    
-#        scale_indices = [3*(x**2) for x in num_boxes]
-#        scale_indices = list(itertools.accumulate(scale_indices, lambda x,y : x+y))
-#    
-#        
-#        li = []
-#        i = 0
-#        for scale in scale_indices:        
-#            li.extend(list(range(i, scale))) 
-#            i = scale
-#        
-#        scale_indices = li
 
     images = args.images
     batch_size = int(args.bs)
@@ -147,36 +127,39 @@ if __name__ ==  '__main__':
         
     load_batch = time.time()
     
-    print("test")
     
-    batches = list(map(prep_image, imlist, [inp_dim for x in range(len(imlist))]))
-    im_batches = [x[0] for x in batches]
-    orig_ims = [x[1] for x in batches]
-    im_dim_list = [x[2] for x in batches]
-    im_dim_list = torch.FloatTensor(im_dim_list).repeat(1,2)
+    imlist.sort()
     
     
+#    batches = list(map(prep_image, imlist, [inp_dim for x in range(len(imlist))]))
+#    im_batches = [x[0] for x in batches]
+#    orig_ims = [x[1] for x in batches]
+#    im_dim_list = [x[2] for x in batches]
+#    im_dim_list = torch.FloatTensor(im_dim_list).repeat(1,2)
     
-    if CUDA:
-        im_dim_list = im_dim_list.cuda()
+    
+    
+#    if CUDA:
+#        im_dim_list = im_dim_list.cuda()
     
     leftover = 0
     
-    if (len(im_dim_list) % batch_size):
+    if (len(imlist) % batch_size):
         leftover = 1
         
         
     if batch_size != 1:
-        num_batches = len(imlist) // batch_size + leftover            
-        im_batches = [torch.cat((im_batches[i*batch_size : min((i +  1)*batch_size,
-                            len(im_batches))]))  for i in range(num_batches)]        
+        num_batches = len(imlist) // batch_size + leftover       
+        im_batches = [list(imlist[i*batch_size : min((i+1)*batch_size), len(imlist)]) for i in range(num_batches)]     
+#        im_batches = [torch.cat((im_batches[i*batch_size : min((i +  1)*batch_size,
+#                            len(im_batches))]))  for i in range(num_batches)]        
 
 
     i = 0
     
 
-    write = False
-    model(get_test_input(inp_dim, CUDA), CUDA)
+#    write = False
+#    model(get_test_input(inp_dim, CUDA), CUDA)
     
     start_det_loop = time.time()
     
@@ -184,11 +167,24 @@ if __name__ ==  '__main__':
     
     
     
-    for batch in im_batches:
+#    for batch in im_batches:
+    for ibatch in range(num_batches):
+        
         #load the image 
         start = time.time()
+        
+        batches = list(map(prep_image, im_batches[ibatch], [inp_dim for x in range(len(im_batches[ibatch]))]))
+        batch = [x[0] for x in batches]
+        orig_ims = [x[1] for x in batches]
+        im_dim_list = [x[2] for x in batches]
+        im_dim_list = torch.FloatTensor(im_dim_list).repeat(1,2)
+        
+        batch = torch.cat(batch)
+        
+        
         if CUDA:
             batch = batch.cuda()
+            im_dim_list = im_dim_list.cuda()
         
 
         #Apply offsets to the result predictions
@@ -197,7 +193,7 @@ if __name__ ==  '__main__':
         # B x (bbox cord x no. of anchors) x grid_w x grid_h --> B x bbox x (all the boxes) 
         # Put every proposed box as a row.
         with torch.no_grad():
-            prediction = model(Variable(batch), CUDA)
+            prediction = model(batch, CUDA)
         
 #        prediction = prediction[:,scale_indices]
 
@@ -229,18 +225,20 @@ if __name__ ==  '__main__':
     
             
           
-        if not write:
-            output = prediction
-            write = 1
-        else:
-            output = torch.cat((output,prediction))
+#        if not write:
+#            output = prediction
+#            write = 1
+#        else:
+#            output = torch.cat((output,prediction))
             
         
         
 
         for im_num, image in enumerate(imlist[i*batch_size: min((i +  1)*batch_size, len(imlist))]):
             im_id = i*batch_size + im_num
-            objs = [classes[int(x[-1])] for x in output if int(x[0]) == im_id]
+#            objs = [classes[int(x[-1])] for x in output if int(x[0]) == im_id]
+            objs = [classes[int(x[-1])] for x in prediction if int(x[0]) == im_id]
+            print("{0:20s} predicted in {1:6.3f} seconds".format(image.split("/")[-1], 
             print("{0:20s} predicted in {1:6.3f} seconds".format(image.split("/")[-1], (end - start)/batch_size))
             print("{0:20s} {1:s}".format("Objects Detected:", " ".join(objs)))
             print("----------------------------------------------------------")
@@ -249,6 +247,35 @@ if __name__ ==  '__main__':
         
         if CUDA:
             torch.cuda.synchronize()
+            
+        
+        scaling_factor = torch.min(inp_dim/im_dim_list,1)[0].view(-1,1)    
+        prediction[:,[1,3]] -= (inp_dim - scaling_factor*im_dim_list[:,0].view(-1,1))/2
+        prediction[:,[2,4]] -= (inp_dim - scaling_factor*im_dim_list[:,1].view(-1,1))/2    
+        prediction[:,1:5] /= scaling_factor
+        
+        for idx in range(prediction.shape[0]):
+            output[idx, [1,3]] = torch.clamp(prediction[idx, [1,3]], 0.0, im_dim_list[idx,0])
+            output[idx, [2,4]] = torch.clamp(prediction[idx, [2,4]], 0.0, im_dim_list[idx,1])
+        colors = pkl.load(open("pallete", "rb"))
+        
+        def write(x, batches, results):
+            c1 = tuple(x[1:3].int())
+            c2 = tuple(x[3:5].int())
+            img = results[int(x[0])]
+            cls = int(x[-1])
+            objectiveness = np.round(float(x[-2]), 2)
+            label = "{0} {1}".format(classes[cls], objectiveness)
+            color = random.choice(colors)
+            cv2.rectangle(img, c1, c2,color, 1)
+            t_size = cv2.getTextSize(label, cv2.FONT_HERSHEY_PLAIN, 1 , 1)[0]
+            c2 = c1[0] + t_size[0] + 3, c1[1] + t_size[1] + 4
+            cv2.rectangle(img, c1, c2,color, -1)
+            cv2.putText(img, label, (c1[0], c1[1] + t_size[1] + 4), cv2.FONT_HERSHEY_PLAIN, 1, [225,255,255], 1)
+        return img
+        
+        list(map(lambda x: write(x, batch, orig_ims), prediction))
+        
     
     try:
         output
@@ -256,32 +283,35 @@ if __name__ ==  '__main__':
         print("No detections were made")
         exit()
         
-    im_dim_list = torch.index_select(im_dim_list, 0, output[:,0].long())
-    
-    scaling_factor = torch.min(inp_dim/im_dim_list,1)[0].view(-1,1)
-    
-    
-    output[:,[1,3]] -= (inp_dim - scaling_factor*im_dim_list[:,0].view(-1,1))/2
-    output[:,[2,4]] -= (inp_dim - scaling_factor*im_dim_list[:,1].view(-1,1))/2
-    
-    
-    
-    output[:,1:5] /= scaling_factor
-    
-    for i in range(output.shape[0]):
-        output[i, [1,3]] = torch.clamp(output[i, [1,3]], 0.0, im_dim_list[i,0])
-        output[i, [2,4]] = torch.clamp(output[i, [2,4]], 0.0, im_dim_list[i,1])
         
         
-    output_recast = time.time()
+        
+#    im_dim_list = torch.index_select(im_dim_list, 0, output[:,0].long())
+#    
+#    scaling_factor = torch.min(inp_dim/im_dim_list,1)[0].view(-1,1)
+#    
+#    
+#    output[:,[1,3]] -= (inp_dim - scaling_factor*im_dim_list[:,0].view(-1,1))/2
+#    output[:,[2,4]] -= (inp_dim - scaling_factor*im_dim_list[:,1].view(-1,1))/2
+#    
+#    
+#    
+#    output[:,1:5] /= scaling_factor
     
-    
-    class_load = time.time()
+#    for i in range(output.shape[0]):
+#        output[i, [1,3]] = torch.clamp(output[i, [1,3]], 0.0, im_dim_list[i,0])
+#        output[i, [2,4]] = torch.clamp(output[i, [2,4]], 0.0, im_dim_list[i,1])
+#        
+#        
+#    output_recast = time.time()
+#    
+#    
+#    class_load = time.time()
 
-    colors = pkl.load(open("pallete", "rb"))
-    
-    
-    draw = time.time()
+#    colors = pkl.load(open("pallete", "rb"))
+#    
+#    
+#    draw = time.time()
 
 
     def write(x, batches, results):
