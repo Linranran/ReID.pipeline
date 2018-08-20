8f44ef7906ce5a22f7f862ac2755e3e1c3fea0b7
Charlie Lam <lin.xiaohan@mail.bnu.edu.cn>
2018-08-20 18:08:31 +0800

    first test

--- detect.py
+++ detect.py
@@ -259,10 +259,10 @@ if __name__ ==  '__main__':
             output[idx, [2,4]] = torch.clamp(prediction[idx, [2,4]], 0.0, im_dim_list[idx,1])
         colors = pkl.load(open("pallete", "rb"))
         
-        def write(x, batches, results):
+        def write(x, orig):
             c1 = tuple(x[1:3].int())
             c2 = tuple(x[3:5].int())
-            img = results[int(x[0])]
+            img = orig[int(x[0])]
             cls = int(x[-1])
             objectiveness = np.round(float(x[-2]), 2)
             label = "{0} {1}".format(classes[cls], objectiveness)
@@ -274,8 +274,9 @@ if __name__ ==  '__main__':
             cv2.putText(img, label, (c1[0], c1[1] + t_size[1] + 4), cv2.FONT_HERSHEY_PLAIN, 1, [225,255,255], 1)
         return img
         
-        list(map(lambda x: write(x, batch, orig_ims), prediction))
-        
+        list(map(lambda x: write(x, orig_ims), prediction))
+        det_names = pd.Series(imlist).apply(lambda x: "{}/det_{}".format(args.det,x.split("/")[-1]))
+        list(map(cv2.imwrite, det_names, orig_ims))
     
     try:
         output
@@ -314,27 +315,27 @@ if __name__ ==  '__main__':
 #    draw = time.time()
 
 
-    def write(x, batches, results):
-        c1 = tuple(x[1:3].int())
-        c2 = tuple(x[3:5].int())
-        img = results[int(x[0])]
-        cls = int(x[-1])
-        objectiveness = np.round(float(x[-2]), 2)
-        label = "{0} {1}".format(classes[cls], objectiveness)
-        color = random.choice(colors)
-        cv2.rectangle(img, c1, c2,color, 1)
-        t_size = cv2.getTextSize(label, cv2.FONT_HERSHEY_PLAIN, 1 , 1)[0]
-        c2 = c1[0] + t_size[0] + 3, c1[1] + t_size[1] + 4
-        cv2.rectangle(img, c1, c2,color, -1)
-        cv2.putText(img, label, (c1[0], c1[1] + t_size[1] + 4), cv2.FONT_HERSHEY_PLAIN, 1, [225,255,255], 1)
-        return img
-    
+#    def write(x, batches, results):
+#        c1 = tuple(x[1:3].int())
+#        c2 = tuple(x[3:5].int())
+#        img = results[int(x[0])]
+#        cls = int(x[-1])
+#        objectiveness = np.round(float(x[-2]), 2)
+#        label = "{0} {1}".format(classes[cls], objectiveness)
+#        color = random.choice(colors)
+#        cv2.rectangle(img, c1, c2,color, 1)
+#        t_size = cv2.getTextSize(label, cv2.FONT_HERSHEY_PLAIN, 1 , 1)[0]
+#        c2 = c1[0] + t_size[0] + 3, c1[1] + t_size[1] + 4
+#        cv2.rectangle(img, c1, c2,color, -1)
+#        cv2.putText(img, label, (c1[0], c1[1] + t_size[1] + 4), cv2.FONT_HERSHEY_PLAIN, 1, [225,255,255], 1)
+#        return img
+#    
             
-    list(map(lambda x: write(x, im_batches, orig_ims), output))
-      
-    det_names = pd.Series(imlist).apply(lambda x: "{}/det_{}".format(args.det,x.split("/")[-1]))
-    
-    list(map(cv2.imwrite, det_names, orig_ims))
+#    list(map(lambda x: write(x, im_batches, orig_ims), output))
+#      
+#    det_names = pd.Series(imlist).apply(lambda x: "{}/det_{}".format(args.det,x.split("/")[-1]))
+#    
+#    list(map(cv2.imwrite, det_names, orig_ims))
     
     end = time.time()
     
@@ -342,12 +343,12 @@ if __name__ ==  '__main__':
     print("SUMMARY")
     print("----------------------------------------------------------")
     print("{:25s}: {}".format("Task", "Time Taken (in seconds)"))
-    print()
-    print("{:25s}: {:2.3f}".format("Reading addresses", load_batch - read_dir))
-    print("{:25s}: {:2.3f}".format("Loading batch", start_det_loop - load_batch))
-    print("{:25s}: {:2.3f}".format("Detection (" + str(len(imlist)) +  " images)", output_recast - start_det_loop))
-    print("{:25s}: {:2.3f}".format("Output Processing", class_load - output_recast))
-    print("{:25s}: {:2.3f}".format("Drawing Boxes", end - draw))
+#    print()
+#    print("{:25s}: {:2.3f}".format("Reading addresses", load_batch - read_dir))
+#    print("{:25s}: {:2.3f}".format("Loading batch", start_det_loop - load_batch))
+#    print("{:25s}: {:2.3f}".format("Detection (" + str(len(imlist)) +  " images)", output_recast - start_det_loop))
+#    print("{:25s}: {:2.3f}".format("Output Processing", class_load - output_recast))
+#    print("{:25s}: {:2.3f}".format("Drawing Boxes", end - draw))
     print("{:25s}: {:2.3f}".format("Average time_per_img", (end - load_batch)/len(imlist)))
     print("----------------------------------------------------------")
 
