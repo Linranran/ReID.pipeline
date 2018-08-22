7ea7b7d79c98a4d9390349d3649fab2d89bfd9a1
Charlie Lam <lin.xiaohan@mail.bnu.edu.cn>
2018-08-22 10:59:07 +0800

    second commit

--- track.py
+++ track.py
@@ -2,25 +2,27 @@ import time
 import numpy as np
 import uuid
 import argparses
+import cv2
 
 import detect
 from preprocess import prep_image
 
 class identity(object):
     colors = pkl.load(open("pallete", "rb"))
-    colors_used = numpy.full(len(colors), False)
+    colors_used = np.full(len(colors), False)
     max_dim = None
     
     def __init__(self):
         colorid = np.random.choice(len(identity.colors))
         while identity.colors_used[colorid]:
             colorid = np.random.choice(len(identity.colors))
-        self.color = identity.colors[colorid]
-        identity.colors_used[colorid] = True
+        self.colorid = colorid
+        self.color = identity.colors[self.colorid]
+        identity.colors_used[self.colorid] = True
         self.name = str(uuid.uuid4())[:8]
         self.hist = []
         self.pos = None
-        self.pos_pre = None
+        self.pos_pre = None     #  used to calculate velocity
         self.interval = 0
         self.v = None
         self.lower_right = None
@@ -119,25 +121,7 @@ def pair_position(position_pre, positions_post, threshold=100):
         return pos_map, False
     else:
         return pos_map, True
-    
-    
-def write_identity(frame, id_list):
-    for i in range(len(id_list)):
-        c1 = tuple(id_list[i].upper_left[0].int(), id_list[i].upper_left[1].int())
-        c2 = tuple(id_list[i].lower_right[0].int(), id_list[i].lower_right[1].int())
-        label = "{0}".format('Person')
-        color = id_list[i].color
-        cv2.rectangle(frame, c1, c2,color, 1)
-        t_size = cv2.getTextSize(label, cv2.FONT_HERSHEY_PLAIN, 1 , 1)[0]
-        c2 = c1[0] + t_size[0] + 3, c1[1] + t_size[1] + 4
-        cv2.rectangle(frame, c1, c2, color, -1)
-        cv2.putText(frame, label, (c1[0], c1[1] + t_size[1] + 4), cv2.FONT_HERSHEY_PLAIN, 1, [225,255,255], 1)
-        
-        for h in range(len(id_list[i].hist)):
-            cv2.circle(frame, id_list[i].hist[h], 3, color, -1)
-        cv2.circle(frame, id_list[i].hist[h], 3, color, -1)
-        
-    return frame
+
     
 
 def kalman_prediction(id_list, Q):
@@ -167,10 +151,16 @@ def kalman_update(id_list, candidate_list, measured_position, R):
     for i in range(len(ttl_list)):
         est_position[i, :] = ttl_list[i].pos
     pos_map, pair = pair_position(est_position, measured_position)
+    
+    accounted = np.full(measured_position.shape[0], False)
+    
     if not pair:
         return id_list, candidate_list
     for i in range(len(pos_map)):
         if pos_map[i] is not None:
+            
+            accounted[[pos_map[i]] = True
+        
             ttl_list[i].K = ttl_list[i].P / (ttl_list[i].P + R)
             ttl_list[i].P -= ttl_list[i].K * ttl_list[i].P
         
@@ -182,16 +172,68 @@ def kalman_update(id_list, candidate_list, measured_position, R):
             ttl_list[i].lower_right[0].clip(a_min=0, a_max=identity.max_dim[0])
             ttl_list[i].lower_right[1].clip(a_min=0, a_max=identity.max_dim[1])
             
+            ttl_list[i].pos_pre = ttl_list[i].pos
+            ttl_list[i].v = (ttl_list[i].pos - ttl_list[i].pos_pre) / ttl_list[i].interval
+            ttl_list[i].interval = 0
+            
             # update candidate_list and id_list
             
             if i >= legit_num:
                 id_list.append(ttl_list[i])
                 candidate_list.remove(ttl_list[i])
+        else:
+            # could be exit ids, add to candidates
+            if i < legit_num:
+                candidate_list.append(ttl_list[i])
+                id_list.remove(ttl_list[i])
+            else:
+                # was in candidate and not detected this time, could be false alarm, or exit ids
+                candidate_list.remove(ttl_list[i])
+                identity.colors_used[ttl_list[i].colorid] = False
+    
+    # deal with new identities
+    for i in range(measured_position.shape[0]):
+        if not accounted[[pos_map[i]]:
+            can_id = identity()
+            can_id.pos = measured_position[i, :2]
+            can_id.pos_pre = can_id.pos
+            can_id.lower_right = measured_position[i, 2:4]
+            candidate_list.append(can_id)
                 
-        # todo: deal with new ids
-        # todo: get velocity
-        # todo: update pos_pre
+    return id_list, candidate_list
             
+            
+def write_identity(frame, id_list):
+    for i in range(len(id_list)):
+        c1 = tuple(id_list[i].upper_left[0].int(), id_list[i].upper_left[1].int())
+        c2 = tuple(id_list[i].lower_right[0].int(), id_list[i].lower_right[1].int())
+        label = "{0}".format('Person')
+        color = id_list[i].color
+        cv2.rectangle(frame, c1, c2,color, 1)
+        t_size = cv2.getTextSize(label, cv2.FONT_HERSHEY_PLAIN, 1 , 1)[0]
+        c2 = c1[0] + t_size[0] + 3, c1[1] + t_size[1] + 4
+        cv2.rectangle(frame, c1, c2, color, -1)
+        cv2.putText(frame, label, (c1[0], c1[1] + t_size[1] + 4), cv2.FONT_HERSHEY_PLAIN, 1, [225,255,255], 1)
+        
+        for h in range(len(id_list[i].hist)):
+            cv2.circle(frame, id_list[i].hist[h], 3, color, -1)
+        cv2.circle(frame, id_list[i].hist[h], 3, color, -1)
+        
+    return frame
+    
+    
+def __show_results__(id_list, candidate_list, image)
+    _img_ = image.copy()
+    
+    for i in range(len(id_list)):
+        
+    
+    
+    cv2.imshow("frame", _img_)
+    key = cv2.waitKey(1)
+    if key & 0xFF == ord('q'):
+        cv2.destroyAllWindows()
+
 
 
 if __name__ ==  '__main__':
@@ -267,6 +309,7 @@ if __name__ ==  '__main__':
     
     for i in range(len(pos_map)):
         id_list[i].pos = velocity[i, 2:4]
+        id_list[i].pos_pre = id_list[i].pos
         id_list[i].v = velocity[i, 0:2]
         id_list[i].lower_right = position_pre[i, 2:4]
     
@@ -275,7 +318,7 @@ if __name__ ==  '__main__':
     # run tracking
     for idx in range(offset, ttl_num):
         start = time.time()
-
+        inter += 1
         frame, ogl, dim = prep_image(imlist[idx], inp_dim)
 
         if inter > interval:
@@ -283,14 +326,15 @@ if __name__ ==  '__main__':
             
         # run kalman filter
         
-        id_list = kalman_prediction(id_list, Q)     # todo
+        id_list = kalman_prediction(id_list, Q)   
         
         if detect_flag:
             measured_positions = detect.measure(frame, dim)[:, 1:5] 
             measured_positions[:, 0] = [(x[0]+x[2])/2 for x in measured_positions]
             measured_positions[:, 1] = [(x[1]+x[3])/2 for x in measured_positions]
             
-            id_list, candidate_list = kalman_update(id_list, candidate_list, measured_positions, R)     # todo
+            id_list, candidate_list = kalman_update(id_list, candidate_list, measured_positions, R)    
+            inter = 0
             
         if idx % 20 == 0:
             [id_list[x].add_hist() for x in range(len(id_list))]
@@ -302,7 +346,6 @@ if __name__ ==  '__main__':
         end = time.time()
         objs = [ids.name for ids in id_list]
         
-        inter += 1
         detect_flag = False
         print("{0:20s} predicted in {1:6.3f} seconds".format(imlist[idx].split("/")[-1], end - start)
         print("{0:20s} {1:s}".format("Identity Detected:", " ".join(objs)))
